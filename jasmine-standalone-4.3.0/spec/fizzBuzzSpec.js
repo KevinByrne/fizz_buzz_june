6be6179c1d2776b8b639247dc28c137e4c5611db
KevinByrne <kevbee79@gmail.com>
2022-07-28 14:49:14 +0100

    further tests and updates

--- jasmine-standalone-4.3.0/spec/fizzBuzzSpec.js
+++ jasmine-standalone-4.3.0/spec/fizzBuzzSpec.js
@@ -40,5 +40,23 @@ describe('Fizzbuzz', function() {
       expect(fizzbuzz.isDivisibleByFive(17)).toBe(false);
     });
   });
+
+  describe('when playing, says', function() {
+    it('"Fizz" when a number is divisible by 3', function() {
+      expect(fizzbuzz.says(3)).toEqual("Fizz");
+    });
+  });
+
+  describe('when playing, says', function() {
+    it('"Buzz" when a number is divisible by 5', function() {
+      expect(fizzbuzz.says(5)).toEqual("Buzz");
+    });
+  });
+
+  describe('when playing says', function() {
+    it('"FizzBuzz" when a number is divisible by 3 & 5', function() {
+      expect(fizzbuzz.says(15)).toEqual("FizzBuzz");
+    });
+  });
 });
 
--- jasmine-standalone-4.3.0/src/fizzBuzz.js
+++ jasmine-standalone-4.3.0/src/fizzBuzz.js
@@ -1,13 +1,30 @@
 class Fizzbuzz {
+  
+  says(number) {
+    if (this.isDivisibleByThreeAndFive(number)) {
+      return "FizzBuzz";
+    }
+    if (this.isDivisibleByThree(number)) {
+      return "Fizz";
+    }
+    if (this.isDivisibleByFive(number)) {
+      return "Buzz";
+    }
+  }
+
   isDivisibleByThree(number) {
-    return (number % 3 === 0);
+    return this._isDivisibleBy (number, 3);
   }
 
   isDivisibleByFive(number) {
-    return (number % 5 === 0);  
+    return this._isDivisibleBy (number, 5);
   }
 
   isDivisibleByThreeAndFive(number) {
-    return (number % 3 === 0) && (number % 5 === 0);
+    return this._isDivisibleBy (number, 15);
+  } 
+
+  _isDivisibleBy(number, divisor) {
+    return (number % divisor === 0);
   }
 }
